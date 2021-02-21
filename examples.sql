b4454e6a941486cb7c94e65f5d475ced798dcdfa
George Myshnick <eprom1234@gmail.com>
2021-02-21 22:11:01 +0300

    more concrete

--- examples.sql
+++ examples.sql
@@ -1,5 +1,7 @@
 
-/* Example: */
+/* Example 1: */
+
+-- this guy is gonna be executed directly
 exec begens '
 
     print @@{Something cool is gonna be executed};
@@ -20,5 +22,28 @@ exec begens '
 
 ';
 
+-- this guy will drop you a normal TSQL code, which you can execute later
+exec begens '
+
+    print @@{Something cool is gonna be executed};
+    
+    exec (@@{
+        print @@{Imagine dynamic queries became more powerful?};
+        
+        exec(@@{
+            
+            print @@{I mean, this is a string, and @@{this is an inner string @@{ and this guy is even more inner} wow 0_0 } o m g};
+            
+            print @@{$${
+                    print @@{Hello world, I'' the first message here};
+                    select @@{And I''m the last};
+                }}
+        });
+    });
+
+', 1; -- <-- see this?
+
 go
 
+/* Example 2: */
+-- place reserved for example 2
